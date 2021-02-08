d1f60d840ce25bc3955868b33fa6cba1bc65cd6e
Felipe Neves <ryukokki.felipe@gmail.com>
2021-02-08 11:42:59 -0300

    yafoc: added interface files include chain

---
+++ src/yaFoC.cpp
@@ -0,0 +1,6 @@
+#include "include/yaFoC/dq_transform.h"
+#include "include/yaFoC/electric_angle.h"
+#include "include/yaFoC/pid_controller.h"
+#include "include/yaFoC/trigonometry_utils.h"
+
+#include "yaFoC.h"
---
+++ src/yaFoC.h
@@ -0,0 +1,5 @@
+#pragma once 
+
+#include "platform_current_sensor.h"
+#include "platform_inverter_driver.h"
+#include "platform_rotor_sensor.h"
